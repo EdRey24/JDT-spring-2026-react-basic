ea6918a8436db4b9607be7475094a6fd48a67b48
Edward Reyna <reynaedward108@gmail.com>
2026-03-19 18:20:44 -0400

    Added Game interface

---
+++ src/interfaces/Game.ts
@@ -0,0 +1,15 @@
+export interface Game {
+    title: string;
+    storeID: string;
+    gameID: string;
+    salePrice: string;
+    normalPrice: string;
+    savings: string;
+    steamRatingText: string;
+    steamRatingPercent: string;
+    steamRatingCount: string;
+    steamAppID: string;
+    releaseDate: number;
+    dealRating: string;
+    thumb: string;
+}
