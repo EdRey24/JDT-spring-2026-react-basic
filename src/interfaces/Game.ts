4ec25f6d55b4bc65486c935c0663f044f142a6cc
Edward Reyna <reynaedward108@gmail.com>
2026-03-21 22:08:05 -0400

    Brought back gameID

--- src/interfaces/Game.ts
+++ src/interfaces/Game.ts
@@ -2,13 +2,13 @@ export interface Game {
     title: string;
     storeID: string;
     dealID: string;
+    gameID: string;
     salePrice: string;
     normalPrice: string;
     savings: string;
     steamRatingText: string;
     steamRatingPercent: string;
     steamRatingCount: string;
-    steamAppID: string;
     releaseDate: number; // seconds since the epoch
     lastChange: number; // seconds since the epoch
     dealRating: string;
